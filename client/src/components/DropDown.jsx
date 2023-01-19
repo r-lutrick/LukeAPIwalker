883222d3d8fa33296b5417a04bd41ca084dabb2e
Robert Lutrick <r.lutrick@icloud.com>
2023-01-18 23:16:14 -0600

    Removed comment

--- client/src/components/DropDown.jsx
+++ client/src/components/DropDown.jsx
@@ -13,16 +13,14 @@ const DropDown = () => {
 
     return (
         <div className="container d-flex align-items-center justify-content-around text-start">
-            {/* <form onSubmit={handleSubmit} className='form-control'> */}
-                <label htmlFor="selectOptions">Search for: </label>
-                <select id="selectOptions" value={select} onChange={(e) => setSelect(e.target.value)}>
-                    <option value="people">People</option>
-                    <option value="planets">Planets</option>
-                </select>
-                <label htmlFor="textForm">ID: </label>
-                <input type="text" id="textForm" onChange={(e) => setText(e.target.value)} />
-                <button onClick={handleSubmit} className="btn btn-dark">Submit</button>
-            {/* </form> */}
+            <label htmlFor="selectOptions">Search for: </label>
+            <select id="selectOptions" value={select} onChange={(e) => setSelect(e.target.value)}>
+                <option value="people">People</option>
+                <option value="planets">Planets</option>
+            </select>
+            <label htmlFor="textForm">ID: </label>
+            <input type="text" id="textForm" onChange={(e) => setText(e.target.value)} />
+            <button onClick={handleSubmit} className="btn btn-dark">Submit</button>
         </div>
     );
 }
